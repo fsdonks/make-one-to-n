dbf1752083089e9edd743021a7c63c7a6e2d70ad
c <you@example.com>
2022-02-01 16:07:46 +0000

    Added script for replacing SRC results in older text files.

---
+++ src_replacer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+#takes a list of values in the key column to delete all of the records in the old file with
+#the SRC records from the new file
+def replace_srcs(old_file, new_file, value_list, out_path, key='SRC',):
+    old_df=pd.read_csv(old_file, sep='\t')
+    #removes old rows
+    old_df=old_df[~old_df[key].isin(value_list)]
+    #replace with new rows
+    new_df=pd.read_csv(new_file, sep='\t')
+    #remove any rows from new_df that don't have a value from value_list in the key column
+    new_df=new_df[new_df[key].isin(value_list)]
+    out_df = pd.concat([old_df, new_df], ignore_index=True)
+    out_df.to_csv(out_path, sep='\t', index=False)
+
+two_periods = "/home/craig/runs/results_no_post_demand/results.txt"
+def test_1():
+    three_periods = "/home/craig/runs/test-run/results.txt"
+    out = "/home/craig/runs/replaced_1.txt"
+    replace_srcs(three_periods, two_periods, ['01205K000'], out)
+
+def test_2():
+    big_phases = "/home/craig/runs/big_test/results_no_truncate_and_1_supply.txt"
+    out = "/home/craig/runs/replaced_2.txt"
+    replace_srcs(big_phases, two_periods, ['01205K000'], out)
+
+def test_3():
+    big_phases = "/home/craig/runs/big_test/results_no_truncate_and_1_supply.txt"
+    no_truncation = "/home/craig/runs/big_test/results.txt"
+    out = "/home/craig/runs/replaced_3.txt"
+    replace_srcs(big_phases, no_truncation, ['01205K000', "01225K000"], out)
+    
+def do_tests():  
+    test_1()
+    test_2()
+    test_3()
+
+do_tests()
+
+
+# In[ ]:
+
+
+
+
