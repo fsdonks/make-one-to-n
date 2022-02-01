4bb22f10063724a3d491d626c2cb2a180d89a70f
c <you@example.com>
2022-02-01 17:31:19 +0000

    Added ability to replace records in the old file with all of the  SRCs in the new file.

--- src_replacer.py
+++ src_replacer.py
@@ -8,16 +8,21 @@ import pandas as pd
 
 #takes a list of values in the key column to delete all of the records in the old file with
 #the SRC records from the new file
+#pass a value_list of False in order to replace all of SRCs in the new records
 def replace_srcs(old_file, new_file, value_list, out_path, key='SRC',):
+    #replace with new rows
+    new_df=pd.read_csv(new_file, sep='\t')
+    if not value_list:
+        value_list=new_df[key].unique()
     old_df=pd.read_csv(old_file, sep='\t')
     #removes old rows
     old_df=old_df[~old_df[key].isin(value_list)]
-    #replace with new rows
-    new_df=pd.read_csv(new_file, sep='\t')
+
     #remove any rows from new_df that don't have a value from value_list in the key column
     new_df=new_df[new_df[key].isin(value_list)]
     out_df = pd.concat([old_df, new_df], ignore_index=True)
     out_df.to_csv(out_path, sep='\t', index=False)
+    print("SRCs being replaced: ", value_list)
 
 two_periods = "/home/craig/runs/results_no_post_demand/results.txt"
 def test_1():
@@ -35,6 +40,8 @@ def test_3():
     no_truncation = "/home/craig/runs/big_test/results.txt"
     out = "/home/craig/runs/replaced_3.txt"
     replace_srcs(big_phases, no_truncation, ['01205K000', "01225K000"], out)
+    #Try passing False as the value_list to replace all SRCs in the new
+    replace_srcs(big_phases, "/home/craig/runs/big_test/results_4_SRCs.csv", False, out)
     
 def do_tests():  
     test_1()
@@ -42,10 +49,5 @@ def do_tests():
     test_3()
 
 do_tests()
-
-
-# In[ ]:
-
-
 
 
