be2191d96d22aa3495b27b866345b2e7bd25dfb7
craig <craig.flewelling@gmail.com>
2023-02-22 19:53:41 +0000

    Made test file location smarter.

--- usage.py
+++ usage.py
@@ -6,9 +6,15 @@ Created on Thu Dec 16 21:08:33 2021
 @author: craig
 """
 
+from pathlib import Path
 import sys
-# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
-sys.path.append('/home/craig/workspace/taa_processor/')
+if '__file__' not in globals():
+    repo_path=Path(__file__).parent
+else: 
+    repo_path='/home/craig/workspace/taa_processor'
+repo_path=repo_path+'/'
+sys.path.append(repo_path)
+
 import taa_post_processing as proc
 
 #Weights used for a weighted score.
@@ -19,7 +25,7 @@ phase_weights= {"comp1" : 0.125,
                "phase3" : .5,
                "phase4" : .125}
 
-resources="/home/craig/workspace/taa_processor/resources/"
+resources=repo_path+"resources/"
 
 results_map = {#'D' : resources+"results_no_truncate_and_1_supply.txt", 
                'C' : resources+"results.txt",
@@ -27,13 +33,13 @@ results_map = {#'D' : resources+"results_no_truncate_and_1_supply.txt",
               }
 
 peak_max_workbook=resources+"computed_maxes.xlsx"
-baseline_path = resources+'/TAA24-28_SRC_BASELINE_201130_DRAFTv6.xlsx'
+baseline_path = resources+'/SRC_BASELINE.xlsx'
 out_location="/home/craig/workspace/taa_processor/"
 proc.make_one_n(results_map, 
                 peak_max_workbook, 
                 out_location, 
                 phase_weights, 
-                'TAA24-28_Modeling_Results.xlsx', 
+                'Modeling_Results.xlsx', 
                 baseline_path, True)
 
 proc.remove_blank_row(out_location+"out_of_order.xlsx", out_location+"out_of_order.xlsx")
