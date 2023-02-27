2207d0bd6762a7c3fa2a7171bde12383e960a1e8
craig <craig.flewelling@gmail.com>
2023-02-27 15:48:50 +0000

    Minor mods to src_replacer
    Added a way to replace a particular branch in a results file.

--- src_replacer.py
+++ src_replacer.py
@@ -9,12 +9,10 @@ import pandas as pd
 #takes a list of values in the key column to delete all of the records in the old file with
 #the SRC records from the new file
 #pass a value_list of False in order to replace all of SRCs in the new records
-def replace_srcs(old_file, new_file, value_list, out_path, key='SRC',):
+def replace_srcs(old_df, new_df, value_list, out_path, key='SRC',):
     #replace with new rows
-    new_df=pd.read_csv(new_file, sep='\t')
     if not value_list:
         value_list=new_df[key].unique()
-    old_df=pd.read_csv(old_file, sep='\t')
     #removes old rows
     old_df=old_df[~old_df[key].isin(value_list)]
 
@@ -22,8 +20,23 @@ def replace_srcs(old_file, new_file, value_list, out_path, key='SRC',):
     new_df=new_df[new_df[key].isin(value_list)]
     out_df = pd.concat([old_df, new_df], ignore_index=True)
     out_df.to_csv(out_path, sep='\t', index=False)
-    print("SRCs being replaced: ", value_list)
+    print("values being replaced: ", value_list)
 
+def replace_srcs_csv(old_file, new_file, value_list, out_path, key='SRC',):
+    new_df=pd.read_csv(new_file, sep='\t')
+    old_df=pd.read_csv(old_file, sep='\t')
+    replace_srcs(old_df, new_df, value_list, out_path, key)
+
+def add_branch(df):
+    df['branch']=df['SRC'].str[:2]
+    
+def replace_branches(old_file, new_file, value_list, out_path):
+    new_df=pd.read_csv(new_file, sep='\t')
+    new_df=add_branch(new_df)
+    old_df=pd.read_csv(old_file, sep='\t')
+    old_df=add_branch(old_df)
+    replace_srcs(old_df, new_df, value_list, out_path, 'branch')
+    
 def resource_path(repo_path, filename):
     return repo_path+"resources/"+filename
 def output_path(repo_path, filename):
@@ -33,24 +46,24 @@ def test_1(repo_path):
     three_periods = resource_path(repo_path, "results_small.txt")
     two_periods = resource_path(repo_path, "results_no_post_demand.txt")
     out = output_path(repo_path, "replaced_1.txt")
-    replace_srcs(three_periods, two_periods, ['01205K000'], out)
+    replace_srcs_csv(three_periods, two_periods, ['01205K000'], out)
 
 def test_2(repo_path):
     big_phases = resource_path(repo_path, 
                                "results_no_truncate_and_1_supply.txt")
     two_periods = resource_path(repo_path, "results_no_post_demand.txt")
     out = output_path(repo_path, "replaced_2.txt")
-    replace_srcs(big_phases, two_periods, ['01205K000'], out)
+    replace_srcs_csv(big_phases, two_periods, ['01205K000'], out)
 
 def test_3(repo_path):
     big_phases = resource_path(repo_path, "results_no_truncate_and_1_supply.txt")
     no_truncation = resource_path(repo_path, "results.txt")
     out = output_path(repo_path, "replaced_3.txt")
-    replace_srcs(big_phases, no_truncation, ['01205K000', "01225K000"], out)
+    replace_srcs_csv(big_phases, no_truncation, ['01205K000', "01225K000"], out)
     #Try passing False as the value_list to replace all SRCs in the new
     four_srcs = resource_path(repo_path, "results_4_SRCs.csv")
     out_2 = output_path(repo_path, "replaced_4.txt")
-    replace_srcs(big_phases, four_srcs, False, out_2)
+    replace_srcs_csv(big_phases, four_srcs, False, out_2)
     
 def do_tests(repo_path):  
     test_1(repo_path)
